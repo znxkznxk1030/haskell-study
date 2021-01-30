a04f53ce2d84b1e95e2a5ef39f9bcb1302bffa5a
ysKim <znxkznxk1030@gmail.com>
2021-01-31 00:34:37 +0900

    checkpoint #1

---
+++ Record.hs
@@ -0,0 +1,11 @@
+module Record (
+Person(..)
+) where
+
+data Person = Person { firstName :: String
+, lastName :: String
+, age :: Int
+, height :: Float
+, phoneNumber :: String
+, flavor :: String
+} deriving (Show)
