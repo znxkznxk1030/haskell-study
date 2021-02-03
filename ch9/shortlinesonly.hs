382dec3838affe6f9804b786dd5cf4cc6f16fecc
ysKim <znxkznxk1030@gmail.com>
2021-02-03 23:01:50 +0900

    add gitignore

---
+++ ch9/shortlinesonly.hs
@@ -0,0 +1,7 @@
+
+main = do
+    contents <- getContents
+	  putStr (shortLinesOnly contents)
+
+shortLinesOnly :: String -> String
+shortLinesOnly = unlines.filter (\line -> length line < 10).lines
