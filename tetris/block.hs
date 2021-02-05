2194f4b3120c28124350887c563f16f4e259af07
Youngsoo 金 <znxkznxk1030@gmail.com>
2021-02-05 15:26:22 +0900

    Update block.hs

--- tetris/block.hs
+++ tetris/block.hs
@@ -5,15 +5,25 @@ module Block (
   genblock,
   getCoord,
   block2coords,
+  randomShape,
   downward
 ) where
+import System.Random
+import Board (width, height)
+import Data.Maybe
 
 data Shape = I | L | J | O | S | Z | T deriving(Eq)
-data Block = Block {shape :: Shape, rotate :: Int, coord :: (Int, Int)} | NONE deriving(Eq)
+data Block = Block {shape :: Shape, rotate :: Int, coord :: (Int, Int)} deriving(Eq)
 
 
-genblock :: Block
-genblock = Block I 0 (5, 2)
+genblock :: Shape -> Block
+genblock shape = Block shape 0 ( 7, 2)
+
+
+randomShape :: IO Shape
+randomShape = do
+  rand <- randomRIO (0, 6) 
+  return ([I, L, J, O, S, Z, T] !! rand)
 
 getShape :: Block -> Shape
 getShape Block{..} = shape
@@ -29,9 +39,33 @@ downward = map (\(x, y) -> (x, y + 1))
 
 block2coords :: Block -> [(Int, Int)]
 block2coords block 
-  | shape == I && rotate == 0 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
-  | shape == I && rotate == 1 = [ (x - 1, y),   (x, y),   (x + 1, y),   (x + 1, y) ]
+  | _shape == I && _rotate == 0 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == I && _rotate == 1 = [ (x - 1, y),   (x, y),   (x + 1, y),   (x + 1, y) ]
+
+  | _shape == L && _rotate == 0 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x + 1, y + 1) ]
+  | _shape == L && _rotate == 1 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == L && _rotate == 2 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == L && _rotate == 3 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+
+  | _shape == J && _rotate == 0 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x - 1, y + 1) ]
+  | _shape == J && _rotate == 1 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == J && _rotate == 2 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == J && _rotate == 3 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+
+  | _shape == O && _rotate == 0 = [ (x, y - 1),   (x + 1, y - 1),   (x, y),   (x + 1, y) ]
+
+  | _shape == S && _rotate == 0 = [ (x, y - 1),   (x + 1, y - 1),   (x, y),   (x - 1, y) ]
+  | _shape == S && _rotate == 1 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+
+  | _shape == Z && _rotate == 0 = [ (x - 1, y - 1),   (x, y - 1),   (x, y),   (x + 1, y) ]
+  | _shape == Z && _rotate == 1 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+
+  | _shape == T && _rotate == 0 = [ (x, y - 1),   (x, y),   (x - 1, y),   (x + 1, y) ]
+  | _shape == T && _rotate == 1 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == T && _rotate == 2 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+  | _shape == T && _rotate == 3 = [ (x, y - 1),   (x, y),   (x, y + 1),   (x, y + 2) ]
+
   | otherwise = []
-  where shape = getShape block
-        rotate = getRotate block
-        (x, y) = getCoord block
+  where _shape = shape block
+        _rotate = rotate block
+        (x, y) = coord block
